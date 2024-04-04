56452b1871bae1369d344dea3f8361cb13b34528
Gian Lucca Paneto <66529207+ogianpaneto@users.noreply.github.com>
2024-04-04 14:07:11 -0300

    ex3 fundamento teoria conjuntos

---
+++ Fundamentos_de_Teoria_dos_Conjuntos/3.py
@@ -0,0 +1,19 @@
+#Exercício 3:
+a = frozenset({'a', 'b', 'c'})
+b = frozenset({'a', ('b','c')})
+c = frozenset({'a', ('b', 'c'), ('d')})
+
+def powerset(conjunto):
+    listaconjunto = list(conjunto)
+    subconjuntos = []
+    for i in range(2 ** len(listaconjunto)):
+        subconjunto = []
+        for k in range(len(listaconjunto)):
+            if i & 1 << k:
+                subconjunto.append(listaconjunto[k])
+        subconjuntos.append(subconjunto)
+    return subconjuntos
+
+print(f'1- {powerset(a)}\n')
+print(f'2- {powerset(b)}\n')
+print(f'3- {powerset(b)}\n')
