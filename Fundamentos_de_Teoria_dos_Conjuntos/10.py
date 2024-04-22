4bce10530bc9da4a5037658b617cff399412286b
Hei-Lima <heitor.lipeixoto@gmail.com>
2024-04-22 18:29:45 -0300

    Adicionado exercício 10 e 9 corrigido..

---
+++ Fundamentos_de_Teoria_dos_Conjuntos/10.py
@@ -0,0 +1,2 @@
+###Esta é uma parte importante do projeto, pois se torna dificil
+###e, creio eu, nada construtivo resolver o exercício com um programa. Leia o txt para entender melhor o processo de resolução.
