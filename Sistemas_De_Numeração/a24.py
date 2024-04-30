ece6e33c293be793232005f97b3cd02afc640c2c
Lucas S Gums <123860836+Evigno@users.noreply.github.com>
2024-04-30 09:05:49 -0300

    a.24.py attempt

---
+++ Sistemas_De_Numeração/a24.py
@@ -0,0 +1,22 @@
+#Solution for exercise a24
+def main():
+    from conversor_bases import conversor
+
+    def test_bases(n1, n2, nr):
+        list = 
+        
+            numeral1 = int(numeral1, base)
+            numeral2 = int(numeral2, base)
+            soma_numerais = numeral1 + numeral2
+
+            conversao = conversor(soma_numerais, base)
+        
+        return base
+
+    print(f'''1. {test_bases('3B8b1', '608b1, 9C1b')} 
+2. {test_bases('93B23b2',  '9A2F8b2',  '10DDHBb2')}
+3. {test_bases('134140b3',  '3124410b3',  '3314100b3')}
+4. {test_bases('6124536b4', '2253134b4', '11411003b4')}''')
+
+if __name__ == '__main__':
+    main()
