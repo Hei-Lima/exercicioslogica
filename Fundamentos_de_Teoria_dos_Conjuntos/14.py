91082920e319673852e177fe5394128f017289d9
Hei-Lima <heitor.lipeixoto@gmail.com>
2024-04-30 22:33:34 -0300

    Corrigido 7 e adicionado 14 (WIP)

--- Fundamentos_de_Teoria_dos_Conjuntos/14.py
+++ Fundamentos_de_Teoria_dos_Conjuntos/14.py
@@ -1,12 +1,10 @@
 import itertools
 
 
-#Exercício 7:
-A = {"p", "q", "r", "s"}
-B = {"r", "t", "v"}
-C = {"p", "s", "t", "u"}
-U = set("w")
-U.update(A, B, C)
+#Exercício 3:
+A = {4, 5, 6, 7, 8}
+B = {8,9, 10, 11}
+f = {(4, 8) (5, 8) (6, 9) (7, 9) (8, 10)}
 
 def absoluteComplement(x):
    z = U - x #U é o conjunto-Universo, crie ele antes.
@@ -16,7 +14,7 @@ def cartesianProduct(x, y):
     z= list(itertools.product(x,y))
     return z
 
-print(f'Exercício 7')
+print(f'Exercício 3')
 print(f'1. {B & C}')
 print(f'2. {A | C }')
 print(f'3. {absoluteComplement(C)}')
