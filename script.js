52f93046f5a7f10c5bbfa81ccaec400026b55803
Gian Lucca Paneto <gianpaneto@gmail.com>
2024-05-04 16:02:34 -0300

    Upload new website

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".menu-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
